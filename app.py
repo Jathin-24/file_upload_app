e927c3c7cb1072deff9aff60854c170ab67cb2c6
Jathin-24 <23jr1a05a4@gmail.com>
2025-06-17 15:09:55 +0530

    Bind to 0.0.0.0 and use Render PORT

--- app.py
+++ app.py
@@ -52,4 +52,6 @@ def download(file_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use Render's PORT or 5000 locally
+    app.run(host='0.0.0.0', port=port)
+
